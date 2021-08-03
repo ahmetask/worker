7ce15cd3b43fd197b4b2c760fc9d4dbb71f22975
Andrei Yangabishev <waffleboot@gmail.com>
2021-08-03 23:35:40 +0300

    worker pool config

---
+++ config.go
@@ -0,0 +1,28 @@
+package worker
+
+type workerPoolConfig struct {
+	maxWorkers       int
+	jobQueueCapacity int
+}
+
+type opts func(*workerPoolConfig)
+
+func WithMaxWorkers(maxWorkers int) opts {
+	return func(cfg *workerPoolConfig) {
+		cfg.maxWorkers = maxWorkers
+	}
+}
+
+func WithJobQueueCapacity(jobQueueCapacity int) opts {
+	return func(cfg *workerPoolConfig) {
+		cfg.jobQueueCapacity = jobQueueCapacity
+	}
+}
+
+func buildWorkerPoolConfig(opts ...opts) *workerPoolConfig {
+	cfg := &workerPoolConfig{}
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	return cfg
+}
